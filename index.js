1a8493f726ba5cd81d081b07b226cf3e36913158
Daniil <daniilbelousov3@gmail.com>
2025-06-19 16:21:50 +0200

    backend

--- index.js
+++ index.js
@@ -4,6 +4,16 @@ const path = require('path');
 const app = express();
 const port = 3000;
 
+// PostgreSQL connection config
+const pool = new Pool({
+    user: 'postgres',
+    host: 'localhost',
+    database: 'devops',
+    password: 'nope',
+    port: 5432, // default PostgreSQL port
+  });
+  
+
 // Serve index.html on the root route
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'public/index.html'));
